ad0d4f2428efb400bfa74ecbc3c862278fa862b1
lopatiy <artemlopatiy.freshcode@gmail.com>
2017-01-30 15:58:31 +0200

    connectors

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,37 @@
 var particles = [];
 
 function setup() {
-    createCanvas(800,800);
+    createCanvas(800, 800);
+    var particlesMap = new Array(width+1);
+
+    for (var i = 0; i < particlesMap.length; i++) {
+        particlesMap[i] = (new Array(height+1));
+    }
+
     fps.start();
 
-    for(var i = 0; i < 200; i++){
-        particles.push(new Particle(random(width), random(height)));
+    for (var j = 0; j < 100; j++) {
+        var x = random(width);
+        var y = random(height);
+
+        particles.push(new Particle(x, y, particlesMap));
     }
 }
 
-function draw(){
+function draw() {
     background(51);
 
     var mouse = createVector(mouseX, mouseY);
     var force;
 
-    for(var i = 0; i < particles.length; i++){
-        if(isMousePressed){
-            force = p5.Vector.sub(mouse, particles[i].position).normalize().mult(-0.003);
+    for (var i = 0; i < particles.length; i++) {
+        if (isMousePressed) {
+            force = p5.Vector.sub(mouse, particles[i].position).normalize().mult(-0.03);
             particles[i].apply(force);
         }
         particles[i].update();
         particles[i].edges(1);
+        particles[i].connectors();
         particles[i].show();
     }
 
--- assets/javascript/particles/particle.js
+++ assets/javascript/particles/particle.js
@@ -1,33 +1,50 @@
-function Particle(x, y) {
-    this.position = createVector(x,y);
-    this.velocity = createVector(0,0);
-    this.acceleration = createVector(0,0);
+function Particle(x, y, particlesMap) {
+    x = Math.abs(Math.floor(x));
+    y = Math.abs(Math.floor(y));
+
+    this.map = particlesMap;
+    this.position = createVector(x, y);
+    this.map[x][y] = 1;
+
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
 
     this.apply = function (force) {
         this.acceleration.add(force);
     };
 
-    this.update = function() {
+    this.update = function () {
         this.velocity.add(this.acceleration);
-        if(this.velocity.mag() > 0.8){
+        if (this.velocity.mag() > 0.8) {
             this.velocity.add(this.velocity.copy().normalize().mult(-0.005));
         }
+
+        try{
+            this.map[Math.abs(Math.floor(this.position.x))][Math.abs(Math.floor(this.position.y))] = undefined;
+        } catch (err){
+            this.map[this.position.x > 800 ? 800 : this.position.x][Math.abs(Math.floor(this.position.y))] = undefined;
+        }
         this.position.add(this.velocity);
+        try {
+            this.map[Math.abs(Math.floor(this.position.x))][Math.abs(Math.floor(this.position.y))] = 1;
+        } catch (err){
+            this.map[this.position.x > 800 ? 800 : this.position.x][Math.abs(Math.floor(this.position.y))] = undefined;
+        }
         this.acceleration.mult(0);
     };
-    
-    this.edges = function (type){
-        if(!type){
-            if(this.position.x < 0 || this.position.x > width){
+
+    this.edges = function (type) {
+        if (!type) {
+            if (this.position.x < 0 || this.position.x > width) {
                 this.position.x = width - this.position.x;
             }
 
-            if(this.position.y < 0 || this.position.y > height){
+            if (this.position.y < 0 || this.position.y > height) {
                 this.position.y = height - this.position.y;
             }
         }
 
-        if(type === 1) {
+        if (type === 1) {
             if (this.position.x < 0 || this.position.x > width) {
                 this.velocity.x = -this.velocity.x;
             }
@@ -38,9 +55,31 @@ function Particle(x, y) {
         }
     };
 
+    this.connectors = function () {
+        var size = 50;
+        var x = Math.abs(Math.floor(this.position.x)) - size / 2;
+        var y = Math.abs(Math.floor(this.position.y)) - size / 2;
+
+        var iBound = size+x > 800 ? 800 : size+x;
+        var jBound = size+y > 800 ? 800 : size+y;
+
+        for (var i = x < 1 ? 0 : x; i < iBound ; i++) {
+            for (var j = y < 1 ? 0 : y; j < jBound; j++) {
+                if (this.map[i][j]) {
+                    this.renderConnector(i, j);
+                }
+            }
+        }
+    };
+
+    this.renderConnector = function (x, y) {
+        stroke(255, 255, 255, 100);
+        line(this.position.x, this.position.y, x, y);
+    };
+
     this.show = function () {
         fill(255);
         noStroke();
         ellipse(this.position.x, this.position.y, 3);
-    }
+    };
 }
